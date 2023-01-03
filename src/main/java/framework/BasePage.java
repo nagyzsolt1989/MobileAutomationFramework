6d78c56bf082c258272fe83451c6d7544f4eae5c
Zsolt Nagy <nagy.zsolt.1989@gmail.com>
2023-01-03 10:35:35 +0100

    Add the basics of page object design pattern

---
+++ src/main/java/framework/BasePage.java
@@ -0,0 +1,32 @@
+package framework;
+
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BasePage {
+
+    private static final int TIMEOUT = 20;
+
+    protected AppiumDriver driver;
+    private WebDriverWait wait;
+
+    public BasePage(AppiumDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(TIMEOUT));
+        PageFactory.initElements(new AjaxElementLocatorFactory(driver, TIMEOUT), this);
+    }
+
+    protected void waitForElementToAppear(By locator) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    protected void waitForElementToDisappear(By locator) {
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+    }
+}
---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import framework.BasePage;
+import io.appium.java_client.AppiumDriver;
+
+public class LoginPage extends BasePage {
+
+    public LoginPage(AppiumDriver driver) {
+        super(driver);
+    }
+}
