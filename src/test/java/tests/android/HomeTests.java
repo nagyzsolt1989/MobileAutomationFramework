07f20cfecf500e9e09424a55d2cb9b2655c4dac9
Zsolt Nagy <nagy.zsolt.1989@gmail.com>
2023-01-10 09:13:56 +0100

    Add Allure report integration

--- src/test/java/tests/android/HomeTests.java
+++ src/test/java/tests/android/HomeTests.java
@@ -2,15 +2,21 @@ package tests.android;
 
 import framework.platform.AndroidBaseTest;
 import io.appium.java_client.AppiumDriver;
+import io.qameta.allure.Description;
 import org.testng.annotations.Test;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Epic;
 import framework.core.BaseTest;
 import pages.PrivacyPage;
 import org.testng.Assert;
 import pages.HomePage;
 
+@Epic("Home")
+@Feature("Layout")
 public class HomeTests extends AndroidBaseTest {
 
     @Test
+    @Description("Verify the layout of the Home page")
     public void verifyHomePage() {
         AppiumDriver driver = BaseTest.getDriver();
 
--- src/test/java/tests/android/LoginTests.java
+++ src/test/java/tests/android/LoginTests.java
@@ -2,7 +2,10 @@ package tests.android;
 
 import framework.platform.AndroidBaseTest;
 import io.appium.java_client.AppiumDriver;
+import io.qameta.allure.Description;
 import org.testng.annotations.Test;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Epic;
 import pages.ForgotPasswordPage;
 import framework.core.BaseTest;
 import pages.PrivacyPage;
@@ -11,9 +14,12 @@ import org.testng.Assert;
 import pages.LoginPage;
 import pages.HomePage;
 
+@Epic("Log In")
+@Feature("Layout")
 public class LoginTests extends AndroidBaseTest {
 
     @Test
+    @Description("Verify the layout of the Log In page")
     public void verifyLogInPage() {
         AppiumDriver driver = BaseTest.getDriver();
 
@@ -33,6 +39,7 @@ public class LoginTests extends AndroidBaseTest {
     }
 
     @Test
+    @Description("Verify the layout of the Forgot Password page")
     public void verifyForgotPasswordPage() {
         AppiumDriver driver = BaseTest.getDriver();
 
