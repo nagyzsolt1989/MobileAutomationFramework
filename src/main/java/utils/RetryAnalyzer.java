331fd73bb67dd15dc9eab3549fa15f9b7e9ac106
Zsolt Nagy <nagy.zsolt.1989@gmail.com>
2023-01-12 17:15:17 +0100

    Add a capability to retry failed cases

---
+++ src/main/java/utils/RetryAnalyzer.java
@@ -0,0 +1,26 @@
+package utils;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyzer implements IRetryAnalyzer {
+
+    private int counter = 0;
+    private static int retryLimit = 3;
+
+    @Override
+    public boolean retry(ITestResult result) {
+        if (!result.isSuccess()) {
+            if (counter < retryLimit) {
+                counter++;
+                result.setStatus(ITestResult.FAILURE);
+                return true;
+            } else {
+                result.setStatus(ITestResult.FAILURE);
+            }
+        } else {
+            result.setStatus(ITestResult.SUCCESS);
+        }
+        return false;
+    }
+}
